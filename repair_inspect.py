902d1a57241a98c630ac41da8284dc14220d5b6e
Adepitan Richard <adepitanrichard4@gmail.com>
2026-05-26 19:52:25 +0100

    Initial commit

---
+++ repair_inspect.py
@@ -0,0 +1,9 @@
+﻿from pathlib import Path
+path = Path('index.html')
+text = path.read_text(encoding='utf-8')
+start = text.find('        <a href="https://wa.me/+2348035713197"')
+print('start', start)
+comment = '<!-- ═════════════════════════════════════='
+end_comment = text.find(comment, start)
+print('comment pos', end_comment)
+print(repr(text[end_comment-40:end_comment+40]))
